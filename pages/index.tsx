3e82b289e0f2f306b8d1e2a1436002f6f943ae31
ksj <tankboya@naver.com>
2022-09-30 16:34:52 +0900

    /

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Seo from "../components/Seo";
+import FriendsSearch from "../components/friendsSearch";
 
 export default function Home() {
   return (
     <>
       <Seo title="Home" />
+      <FriendsSearch />
     </>
   );
 }
